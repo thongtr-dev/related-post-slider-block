efdddcefdaeab2f4f101414178b6caeac9b91d37
thongtr-dev <thongtr.dev@gmail.com>
2022-11-30 15:27:13 +0700

    New index.asset.php & index.js build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'dfa1e5a2cdd40c7096da');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => '6db65724b6f71be9a1ef');
